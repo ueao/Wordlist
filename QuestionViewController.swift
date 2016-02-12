bf2390cbd75c44c7bac569c90b0f7a2c65009263
Aoi Sakaue <aoiue.w.bbb@gmail.com>
2016-02-12 10:04:32 +0900

    単語帳のアプリ

---
+++ QuestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuestionViewController.swift
+//  Wordlist
+//
+//  Created by Aoi Sakaue on 2016/02/12.
+//  Copyright © 2016年 Aoi Sakaue. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Wordlist/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  Wordlist
+//
+//  Created by Aoi Sakaue on 2016/02/12.
+//  Copyright © 2016年 Aoi Sakaue. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
